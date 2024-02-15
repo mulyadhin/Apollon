c37486209b1674090220019e5bd61cf4c19774c7
mulyadhin <158083973+mulyadhin@users.noreply.github.com>
2024-02-15 19:39:04 +0100

    axios added and city search linked

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.daily[0].temperature.day;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = `9b523c80eead3e092o98fa4bbt7a0e84`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function searchRequest(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
